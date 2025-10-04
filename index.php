f6fb41a7ae2210318abce739b1a4acccf9ae359f
Adbaj <adsesbaj@gmail.com>
2025-10-04 22:02:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>EC2 Instance Page</title>
+</head>
+<body>
+  <h1>Hello from EC2 Instance!</h1>
+  <p>This server's public IP is: <?php echo $_SERVER['SERVER_ADDR']; ?></p>
+</body>
+</html>
