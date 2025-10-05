e32ce0c0e5a0c6db30fcd2ed890b5ab81f368198
Adbaj <adsesbaj@gmail.com>
2025-10-05 11:53:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
   <title>EC2 Instance Page</title>
 </head>
 <body>
-  <h1>Hello from EC2 Instance!</h1>
-  <p>This server's public IP is: <?php echo $_SERVER['SERVER_ADDR']; ?></p>
+  <h1>Hello from: <?php echo $_SERVER['SERVER_ADDR']; ?></h1>
 </body>
 </html>
